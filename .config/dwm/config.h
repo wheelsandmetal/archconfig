d8dbb5b0c2d3435dcd17fd4fd859887e5f30f6c9
wheelsandmetal <jakob@schmutz.co.uk>
2017-12-07 15:59:29 +0000

    Updated for urxvt settings

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -57,14 +57,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "/home/jakob/.config/st/st", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char *webcmd[]   = { "chromium", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,			XK_w,       spawn,          {.v = webcmd } },
+	{ MODKEY,			XK_w,      spawn,          {.v = webcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
